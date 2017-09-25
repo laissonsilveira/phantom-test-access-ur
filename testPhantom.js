0b81bcab8d5a028e883d458240a7ff0619b81c6f
laisson.silveira <laisson.silveira@digitro.com.br>
2017-09-25 12:12:28 -0300

    Melhorias

--- testPhantom.js
+++ testPhantom.js
@@ -3,7 +3,9 @@ var url = 'http://esaj.tjac.jus.br/cpopg/open.do';
 page.open(url, function(status) {
   console.log("Status: " + status);
   if(status === "success") {
-    page.render('esaj.png');
+  	var img = 'screenshots/' + new Date().getTime() + '.png';
+    page.render(img);
+    console.log("Screenshot saved: " + img);
   }
   phantom.exit();
 });
