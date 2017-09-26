d173d28afa7142c2820658ab2dc7174041264ab3
Laisson R. Silveira <laisson.r.silveira@gmail.com>
2017-09-26 10:34:25 -0300

    Commit...

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 
 Execução com pacote personalizado
 
-	./3rd/phantomjs-2.1.1 --config='config.json' testPhantom.js
+	./3rd/phantomjs-2.1.1 --config='config.json' testPhantom.js > http.log
 
 Execução com Phantom mais atual (sudo apt-get install phantomjs)
 
-	phantomjs --config='config.json' testPhantom.js
+	phantomjs --config='config.json' testPhantom.js > http.log
 
 Teste a URL no Chrome
 	
--- testPhantom.js
+++ testPhantom.js
@@ -1,12 +1,19 @@
 var page = require('webpage').create();
-var system = require('system');
 var url = 'http://esaj.tjac.jus.br/cpopg/open.do';
-page.open(url, function(status) {
-  console.log("Status: " + status);
-  if(status === "success") {
-  	var img = 'screenshots/' + new Date().getTime() + '.png';
-    page.render(img);
-    console.log("Screenshot saved: " + img);
-  }
-  phantom.exit();
+
+page.onResourceRequested = function (request) {
+    console.log('REQUEST: ' + JSON.stringify(request, undefined, 4));
+};
+page.onResourceReceived = function (response) {
+    console.log('RECEIVE: ' + JSON.stringify(response, undefined, 4));
+};
+// page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36';
+page.open(url, function (status) {
+    console.log("STATUS: " + status);
+    if (status === "success") {
+        var img = 'screenshots/' + new Date().getTime() + '.png';
+        page.render(img);
+        console.log("Screenshot saved: " + img);
+    }
+    phantom.exit();
 });
